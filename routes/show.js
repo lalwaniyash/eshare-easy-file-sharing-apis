98e64ced0bfd7ecec2b038cf50c9c61569a9f5d3
lalwaniyash <77988031+lalwaniyash@users.noreply.github.com>
2022-10-13 00:48:16 +0530

    Update show.js

--- routes/show.js
+++ routes/show.js
@@ -12,7 +12,7 @@ router.get('/:uuid', async (req, res) => {
             uuid: file.uuid,
             fileName: file.filename,
             fileSize: file.size,
-            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+            downloadLink: `${process.env.APP_BASE_URL}files/download/${file.uuid}`
             
         });
     } catch(err) {
