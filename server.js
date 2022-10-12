5510356866aeb8ebfdef520b46f83f9448256133
lalwaniyash <77988031+lalwaniyash@users.noreply.github.com>
2022-10-13 01:22:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 // Cors 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
