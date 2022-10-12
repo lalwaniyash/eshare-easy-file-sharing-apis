8f4c9a983b32a7c0f843c4b3fe1900e71f87a3dd
lalwaniyash <lalwaniy388@gmail.com>
2022-10-13 00:26:01 +0530

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
