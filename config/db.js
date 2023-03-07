2fae0fe021ecfd4f91abc95a7ec81f757ab3349e
lalwaniyash <77988031+lalwaniyash@users.noreply.github.com>
2023-03-07 16:26:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -14,15 +14,9 @@ const connectionParams = {
 };
 
 function connectDB() {
-mongoose
-.connect (process.env.MONGO_CONNECTION_URL, connectionParams)
-.then( () => {
-    console.info ("Connected to the DB");
-})
-
-.catch((e) => {
-console.log ("Error:", e)
-});
+    mongoose.connect(process.env.MONGO_CONNECTION_URL).then( ()=> {
+        console.log(`connection successful`); 
+    }).catch((err)=> console.log(`no connection`));
 }
 
 
